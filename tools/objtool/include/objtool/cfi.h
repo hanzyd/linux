56d83a7da620646635dc54e74249cba4b4134c84
Madhavan T. Venkataraman <madvenka@linux.microsoft.com>
2022-05-11 01:13:58 -0500

    objtool: Reorganize CFI code
    check.c implements static stack validation. But the CFI code that it
    contains can be shared with other types of validation. E.g., dynamic FP
    validation. Move the CFI code to its own files - cfi.h and cfi.c.
    
    Signed-off-by: Madhavan T. Venkataraman <madvenka@linux.microsoft.com>

--- tools/objtool/include/objtool/cfi.h
+++ tools/objtool/include/objtool/cfi.h
@@ -37,4 +37,16 @@ struct cfi_state {
 	bool end;
 };
 
+void init_cfi_state(struct cfi_state *cfi);
+struct cfi_state *cfi_hash_find_or_add(struct cfi_state *cfi);
+void cfi_hash_add(struct cfi_state *cfi);
+void *cfi_hash_alloc(unsigned long size);
+bool cficmp(struct cfi_state *cfi1, struct cfi_state *cfi2);
+void set_func_state(struct cfi_state *state);
+
+extern unsigned long nr_cfi, nr_cfi_reused, nr_cfi_cache;
+extern struct cfi_init_state initial_func_cfi;
+extern struct cfi_state init_cfi;
+extern struct cfi_state func_cfi;
+
 #endif /* _OBJTOOL_CFI_H */
